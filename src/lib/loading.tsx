ac11110003cd26a5a04628548adb99a131e5b000
Feekkk <zxduke350@gmail.com>
2026-08-21 09:10:25 +0800

    update: loading components

--- src/lib/loading.tsx
+++ src/lib/loading.tsx
@@ -3,7 +3,7 @@ import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 export function LoadingScreen() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-background">
-      <DotLottieReact src="/receipt.json" loop autoplay className="h-48 w-48" />
+      <DotLottieReact src="/loading.json" loop autoplay className="h-48 w-48" />
     </div>
   );
 }
